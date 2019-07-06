ae9ce92a6732e88df6931f8cdbb75b70db5a48d2
Rahul-21 <36933164+Rahul-21@users.noreply.github.com>
2019-07-06 11:17:20 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,59 @@
+onst express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(config.db, { useNewUrlParser: true });
+mongoose.connection.on('connected', () => {
+    console.log('Connected to Database '+ config.db);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Database error '+err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+const comments = require('./routes/comments');
+
+// Port Number
+const port = process.env.PORT || 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// var passport = require('./config/passport')(passport);
+app.get('/',(req,res)=>{
+    return res.send('index.js')
+})
+app.use('/users', users);
+app.use('/comments', comments);
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('invaild endpoint');
+});
+
+app.get('*', (req, res) => {
+    res.redirect('/');
+    // res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Start Server
+app.listen(port, () => {
+    console.log('Server started on port '+port);
+});
